16dab6d1d9cb7ca68a33267059ad4b194dd21e08
David Igandan <davidigandan@gmail.com>
2024-10-14 14:25:34 +0100

    Selection Title doesn't control UI

--- src/components/MySelectionCanvas.tsx
+++ src/components/MySelectionCanvas.tsx
@@ -8,50 +8,51 @@ import {
   Zoom,
   Selection,
   Pan,
-} from '@h5web/lib'
-import { useThrottledState } from '@react-hookz/web';
+} from "@h5web/lib";
+import { useThrottledState } from "@react-hookz/web";
 
 function getTitleForSelection(selection: Rect | undefined) {
-if (!selection) {
-  return 'No selection';
-}
+  if (!selection) {
+    return "No selection";
+  }
 
-const [start, end] = selection;
-return `Selection from (${start.x}, ${start.y}) to (${end.x}, ${end.y})`;
+  const [start, end] = selection;
+  return `Selection from (${start.x.toFixed(3)}, ${start.y.toFixed(
+    3
+  )}) to (${end.x.toFixed(3)}, ${end.y.toFixed(3)})`;
 }
-
 export function MySelectionCanvas() {
-const [activeSelection, setActiveSelection] = useThrottledState<
-  Selection | undefined
->(undefined, 50);
+  const [activeSelection, setActiveSelection] = useThrottledState<
+    Selection | undefined
+  >(undefined, 50);
 
-return (
-  <VisCanvas
-    title={getTitleForSelection(activeSelection?.data)}
-    abscissaConfig={{ visDomain: [-10, 0], showGrid: true }}
-    ordinateConfig={{ visDomain: [50, 100], showGrid: true }}
-  >
-    <Pan modifierKey={'Control'} />
-    <Zoom />
-    <ResetZoomButton />
-
-    <SelectionTool
-      onSelectionChange={setActiveSelection}
-      onSelectionEnd={() => setActiveSelection(undefined)}
+  return (
+    <VisCanvas
+      title={getTitleForSelection(activeSelection?.data)}
+      abscissaConfig={{ visDomain: [-10, 0], showGrid: true }}
+      ordinateConfig={{ visDomain: [50, 100], showGrid: true }}
     >
-      {({ html: htmlSelection }, _, isValid) => (
-        <SvgElement>
-          <SvgRect
-            coords={htmlSelection}
-            fill={isValid ? 'teal' : 'orangered'}
-            fillOpacity={0.5}
-            stroke={isValid ? 'teal' : 'orangered'}
-            strokeWidth={2}
-            strokePosition="inside"
-          />
-        </SvgElement>
-      )}
-    </SelectionTool>
-  </VisCanvas>
-);
+      <Pan modifierKey={"Control"} />
+      <Zoom />
+      <ResetZoomButton />
+
+      <SelectionTool
+        onSelectionChange={setActiveSelection}
+        onSelectionEnd={() => setActiveSelection(undefined)}
+      >
+        {({ html: htmlSelection }, _, isValid) => (
+          <SvgElement>
+            <SvgRect
+              coords={htmlSelection}
+              fill={isValid ? "teal" : "orangered"}
+              fillOpacity={0.5}
+              stroke={isValid ? "teal" : "orangered"}
+              strokeWidth={2}
+              strokePosition="inside"
+            />
+          </SvgElement>
+        )}
+      </SelectionTool>
+    </VisCanvas>
+  );
 }
